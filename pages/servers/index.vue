ad03aad09c0fb33755268c628c4b6be000ed08e0
oussema-attia <oussema.attia@keyrus.com>
2023-06-26 11:59:36 +0100

    update list servers

--- pages/servers/index.vue
+++ pages/servers/index.vue
@@ -1,8 +1,8 @@
 <template>
-  <h2>List Servers</h2>
-  <a-button type="primary" @click="navigateTo(localePath('servers-add'))"
-    >Add server</a-button
-  >
+  <h2>{{ $t("list") }}</h2>
+  <a-button type="primary" @click="navigateTo(localePath('servers-add'))">{{
+    $t("add")
+  }}</a-button>
   <a-table :dataSource="servers" :columns="columns">
     <template #bodyCell="{ column, record }">
       <template v-if="column.key === 'isActive'">
@@ -24,9 +24,9 @@
           >
           <a-divider type="vertical" />
           <a-popconfirm
-            title="Are you sure ?"
-            ok-text="Yes"
-            cancel-text="No"
+            :title="$t('titlePop')"
+            :ok-text="$t('yes')"
+            :cancel-text="$t('no')"
             @confirm="useDeleteServer(record.id)"
           >
             <a>{{ $t("delete") }}</a>
@@ -40,14 +40,14 @@
 import { useServerStore } from "@/stores/store";
 const store = useServerStore();
 const localePath = useLocalePath();
+const i18n = useI18n();
 const servers = computed(() => {
   return store.servers;
 });
 const useDeleteServer = (id: number) => {
   store.useDeleteServer(id);
-  useOpenNotification("Success delete", "success");
+  useOpenNotification(i18n.t("SuccessDelete"), "success");
 };
-const i18n = useI18n();
 const columns = [
   {
     title: i18n.t("serverId"),
